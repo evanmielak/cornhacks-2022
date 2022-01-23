f52967b0eec503a15eaf8e66b763a5b7c4d5b1e2
CadenFries <friescaden@gmail.com>
2022-01-23 03:35:44 -0600

    Barrel Logic

--- scenes/Barrel.gd
+++ scenes/Barrel.gd
@@ -13,3 +13,8 @@ func _physics_process(delta):
 	velocity.y += 20
 	velocity.x = 50 * direction
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		get_tree().change_scene("res://scenes/Main_4.tscn")
