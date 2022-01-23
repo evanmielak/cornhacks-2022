b60e519793fc7277ecace359ed16d4eb54e2f1b0
Evan Mielak <evanmielak@gmail.com>
2022-01-23 02:19:10 -0600

    Added text tutorial level 1, added transition to level 2.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -48,3 +48,8 @@ func _physics_process(delta):
 	if velocity.y != 0:
 		_animated_sprite.play("Jump")
 				
+
+
+func _on_LevelTransition_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().change_scene("res://scenes/Main_2.tscn")
